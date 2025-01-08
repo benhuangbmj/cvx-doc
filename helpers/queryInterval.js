0e1844591a28485ed063733b7eb4ebddd95a1e9a
benhuangbmj <benjaminwong1985@gmail.com>
2025-01-08 11:17:15 -0500

    chore: recover the helper function

---
+++ helpers/queryInterval.js
@@ -0,0 +1,15 @@
+export default function queryInterval(target, method, interval = 100) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const node = method(target);
+      if (node && !node.dataset?.queried) {
+        node.setAttribute("data-queried", true);
+        resolve(node);
+      } else if (node?.dataset?.queried) {
+        reject("queried");
+      } else {
+        reject("not found");
+      }
+    }, interval);
+  });
+}
